f7e6618fa862ef848bf9d6510ed036b41af82aa1
Raj Singh <rajsingh@rajsingh.dev.box.net>
2019-06-15 15:25:59 -0700

    Update permissions to build.sh 	modified:   Dockerfile 	modified:   Jenkinsfile 	new file:   ansible/ansible.cfg 	new file:   ansible/plays/test.yml 	new file:   ansible/requirements.txt 	modified:   build.sh

--- build.sh
+++ build.sh
@@ -8,12 +8,16 @@ mkdir -p $godir/box-node-alert-worker
 export GOPATH=/tmp/go
 
 mv cmd $godir/box-node-alert-worker/
+mv options $godir/box-node-alert-worker/
 mv pkg $godir/box-node-alert-worker/
+mv workerpb $godir/box-node-alert-worker/
+
 cd $godir/box-node-alert-worker
-mkdir bin
 go get ./...
-go test -v ./pkg/...
-CGO_ENABLED=1 GOOS=linux go build -o bin/node-alert-worker cmd/node-alert-worker.go
+ls -la
+mkdir bin
+go get github.com/sirupsen/logrus
+CGO_ENABLED=0 GOOS=linux go build -o bin/node-alert-worker -ldflags '-w' cmd/node-alert-worker.go
 
 mkdir -p /git-root/build
 mv bin/node-alert-worker /git-root/build
