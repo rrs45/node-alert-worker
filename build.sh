451db1d6b7b344192a9b5b9d44e2b605743a3b22
Raj Singh <rajsingh@rajsingh.dev.box.net>
2019-10-01 11:09:53 -0700

    Updating build 	modified:   Jenkinsfile 	modified:   build.sh 	new file:   go.mod 	new file:   go.sum
    	modified:   Buildfile

--- build.sh
+++ build.sh
@@ -12,10 +12,14 @@ mv cmd $godir/box-node-alert-worker/
 mv options $godir/box-node-alert-worker/
 mv pkg $godir/box-node-alert-worker/
 mv workerpb $godir/box-node-alert-worker/
+mv go.mod $godir/box-node-alert-worker/
 
+#go get k8s.io/klog && cd $GOPATH/src/k8s.io/klog && git checkout v0.4.0
 cd $godir/box-node-alert-worker
+export GO111MODULE=on
 go get ./...
 ls -la
+cat go.mod
 mkdir bin
 go get github.com/sirupsen/logrus
 CGO_ENABLED=0 GOOS=linux go build -o bin/node-alert-worker -ldflags '-w' cmd/node-alert-worker.go
